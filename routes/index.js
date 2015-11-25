8e81125a2b80648c06f32f79bf435a3d28ccada0
zhun <zhun@huoli.com>
2015-11-25 20:18:47 +0800

    add express-hbs as express template view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
+var toDoList = require('../lib/toDoList');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next){
+  res.render('toDolist');
 });
 
 module.exports = router;
